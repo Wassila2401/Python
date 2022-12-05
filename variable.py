912de3efbcb4da2e1706a8cd7f2cbea382855277
WassBouss <wassila239606boussetaoui@gmail.com>
2022-12-05 10:53:41 +0100

    premier commit
    cours sur les variables

---
+++ variable.py
@@ -0,0 +1,29 @@
+   # l'operationd'affection =permet d'injecter une valeur dans une varéable
+
+my_text1 = "ceci est une chaine de caractères"
+my_text2 ='ceci est aussi une chaine de caractères'
+
+print(my_text1)
+print(my_text2)
+
+
+my_number1 =142
+my_number2 =65
+
+print(my_number1 -my_number2)
+
+# float, nombre a  virgule flottante
+my_number3 = 3.14
+my_number4 =-2.71
+my_number5 = 0.0
+
+print(my_number3)
+print(my_number4)
+print(my_number5)
+print(type(my_number5))
+print(type(my_number))
+
+
+# ctrl K T = ouvrir la gestion des thèmes
+
+   
